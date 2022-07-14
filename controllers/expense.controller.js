6e46002af261377840b828fb7a0409b7d4f174b5
vaso bazuashvili <v.bazuashvili.websmart@gmail.com>
2022-07-14 12:19:34 +0400

    added new create feature

--- controllers/expense.controller.js
+++ controllers/expense.controller.js
@@ -8,3 +8,22 @@ exports.get = async (req, res) => {
       res.status(422).send({answer: err});
     }
   }
+
+  exports.add = async (req, res) => {
+    const { shop } = req.body;
+    const { cost } = req.body;
+    if (!shop || !cost) {
+      res.status(422);
+      return res.send({answer: "Shop name or price input is not defined."});
+    }  
+    if (cost < 0 && isNaN(cost)) {
+        res.status(422);
+        return res.send({answer: "Cost must be a positive number!"});
+    }
+    try {
+      const newInstance = await expanse.create(req.body);
+      return newInstance && (await this.get(req, res));
+    } catch (error) {
+      return res.status(422).send({ answer: error })
+    }
+  }
